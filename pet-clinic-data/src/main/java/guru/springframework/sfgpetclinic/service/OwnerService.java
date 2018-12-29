55bd5b22b49d846ecba092c3fc42da6fb18740f1
krlosh <krlosh@gmail.com>
2018-12-29 20:28:52 +0100

    Tag service implementations as spring beans

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/OwnerService.java
@@ -3,7 +3,6 @@ package guru.springframework.sfgpetclinic.service;
 import guru.springframework.sfgpetclinic.model.Owner;
 
 import java.util.Set;
-
 public interface OwnerService  extends CrudService<Owner, Long>{
 
     Set<Owner> findByLastName(String lastName);
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/service/PetService.java
@@ -2,7 +2,6 @@ package guru.springframework.sfgpetclinic.service;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 
-
 public interface PetService extends CrudService<Pet, Long> {
 
 }
